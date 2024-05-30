0ddc0f5c04895a809bffa856d04944a07c4af838
byHYONS <byhyons@gmail.com>
2024-05-30 16:20:38 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+
+export default {
+  nome: AppBoolando,
+  data(){
+    return{
+
+    }
+  },
+  components: {
+
+  }
+}
+
+// STRUTTURA APP
+</script>
+
+
+<template>
+  
+
+</template>
+
+
+<style lang="sass">
+
+</style>
